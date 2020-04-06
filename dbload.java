6a0e7e5c3a4d8dc4aac1884114f864c6fb64ab0d
Ryan Babij <rbabij@gmail.com>
2020-04-06 19:47:27 +1000

    Arg parsing
    Add pageSize and filename arg parsing

--- dbload.java
+++ dbload.java
@@ -1,8 +1,50 @@
 public class dbload {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+    	
+    	int pageSize = 0;
+    	String file = "";
+
+        System.out.println("CSV heap database loader");
+        
+        if (args.length!=2)
+        {
+        	System.out.println("Error: Program must provide input file and pagesize arguments.");
+        }
+        else
+        {
+           file = args[0];
+           
+           if (file.length()==0)
+           {
+        	   System.out.println("Error: Bad filename arg.");
+        	   System.exit(1);
+           }
+           
+           System.out.println("Reading file: "+file);
+           try
+           {
+                pageSize = Integer.parseInt(args[1]);
+            }
+            catch (NumberFormatException nfe)
+           {
+                System.out.println("Error: Pagesize must be an integer.");
+                System.exit(1);
+            }
+
+        }
+        
+        // check that pageSize is valid.
+        
+        if ( pageSize <= 0 )
+        {
+        	System.out.println("Error: Pagesize must be greater than 0");
+        	System.exit(1);
+        }
+        
+        // From here we should have valid args. Begin loading the file into the heap.
+        
+        
     }
 
 }
