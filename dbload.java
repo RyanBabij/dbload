25713da6eddc6105e03ff40e7a0bbf29fb44673c
Ryan Babij <rbabij@gmail.com>
2020-04-06 20:35:34 +1000

    Add string/int field check

--- dbload.java
+++ dbload.java
@@ -59,9 +59,7 @@ public class dbload {
         	
         	// ignore newlines. commas mean next column (nColumns is hardcoded)
         	
-        	// create the pagefile to build
-        	//int remainingBytes = pageSize;
-        	//int aByte[pageSize];
+
         	int aByte[]=new int[pageSize];
         	int indexByte=0; // current location on page array
         	
@@ -73,7 +71,44 @@ public class dbload {
             {
             	if (byteRead == ',')
             	{
-            		System.out.println("STOP... COMMATIME");
+            		// comma found, which delimits the data. From here the data must be
+            		// determined to be either an int or a string.
+            		// CSVs sometimes escape commas using "," however the given data
+            		// does not seem to have these errant commas.
+            		
+            		System.out.println("STOP... COMMATIME. Checking loaded field.");
+            		// process the data here, including nulls.
+            		// my binary file will use commas to delimit the data in the heap,
+            		// so if it's a string, we can just copy it as-is.
+
+            		// if the data is a number, it is 4 bytes, plus 1 byte for delimiter
+            		// if the data is a string, it is string.length()+ 1 byte for the delimiter
+            		
+            		// Check if vector data is a string or ASCII number.
+            		boolean isNumeric=true;
+            		for (int i:vCurrentField)
+            		{
+            			if (i>=48 && i<=57)
+            			{}
+            			else
+            			{
+            				isNumeric=false;
+            				break;
+            			}
+            		}
+            		
+            		if ( isNumeric )
+            		{
+            			System.out.println("Field is an integer");
+            			// push integer to page, or overflow to next page.
+            		}
+            		else
+            		{
+            			System.out.println("Field is a string");
+            			// push string to page, or overflow to next page.
+            		}
+            		vCurrentField.clear();
+            		
             	}
             	else if (byteRead == '\n' || byteRead == '\r')
             	{
@@ -81,12 +116,13 @@ public class dbload {
             	}
             	else
             	{
-                	// push to vector until we find a comma. Then check if it will fit onto page.
+                	// push to vector until we find a comma
                 	vCurrentField.add(byteRead);
                 	System.out.println("DATA");
             	}
 
             }
+            System.out.println("Process final field here");
         }
         catch (IOException ex)
         {
